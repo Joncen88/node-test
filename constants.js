fcf8f418e6744d982410bdada94065a02f52e0cd
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-14 11:27:13 +0700

    added new functions to index and const files

--- constants.js
+++ constants.js
@@ -1,3 +1,6 @@
-const timezone = 'Indochina'
+export const timezone = 'Indochina'
 
 export default timezone 
+
+export const pi = 3.14
+export const tau = 6.28
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import{timezone} from './constants.js'
+import{pi, tau} from './constants.js'
+
 console.log(timezone)
+console.log(pi)
+console.log(tau)
