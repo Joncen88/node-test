b6789413a9a5e8650571a551d372a9ece9da4455
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-16 11:20:47 +0700

    added db link

--- db.js
+++ db.js
@@ -7,8 +7,7 @@ const db = new Pool({
   ssl: {
     rejectUnauthorized: false
   },
-  connectionString:
-    'postgres://joncen:7wOZQb6uiBNNTZlQfo8WD6AxE98OVntH@dpg-cmsa5i6d3nmc73eqd01g-a.singapore-postgres.render.com/dbprojects_9809'
+  connectionString: 'lsdjflajfdls'
 })
 
 export default db
---
+++ secrets.js
@@ -0,0 +1,2 @@
+export const DBURL =
+  'postgres://joncen:7wOZQb6uiBNNTZlQfo8WD6AxE98OVntH@dpg-cmsa5i6d3nmc73eqd01g-a.singapore-postgres.render.com/dbprojects_9809'
